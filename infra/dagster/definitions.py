03188071a7a0aaebb2decac2ba5a8d82f42cdae9
Igor Yermako <shingrus@gmail.com>
2026-03-03 23:02:14 +0000

    + build asn schedule

--- infra/dagster/definitions.py
+++ infra/dagster/definitions.py
@@ -4,7 +4,10 @@ from infra.dagster.build_data_job import geofeed_finder_job, pdb_asn_geo_job, pa
 from infra.dagster.build_asn_data_job import build_asn_data_job
 from infra.dagster.build_geo_database_job import build_geo_database_job
 from infra.dagster.create_new_rdns_rules_job import create_new_rdns_rules_job
-from infra.dagster.schedules import create_new_rdns_rules_hourly_schedule
+from infra.dagster.schedules import (
+    build_asn_data_hourly_schedule,
+    create_new_rdns_rules_hourly_schedule,
+)
 
 
 defs = Definitions(
@@ -17,6 +20,7 @@ defs = Definitions(
     ],
     resources={"paths": paths},
     schedules=[
+        build_asn_data_hourly_schedule,
         create_new_rdns_rules_hourly_schedule,
     ],
 )
--- infra/dagster/schedules.py
+++ infra/dagster/schedules.py
@@ -1,8 +1,35 @@
 from dagster import schedule
 
+from infra.dagster.build_asn_data_job import build_asn_data_job
 from infra.dagster.create_new_rdns_rules_job import create_new_rdns_rules_job
 
 
 @schedule(cron_schedule="30 */12 * * *", job=create_new_rdns_rules_job)
 def create_new_rdns_rules_hourly_schedule():
     return {}
+
+
+@schedule(cron_schedule="30 */12 * * *", job=build_asn_data_job)
+def build_asn_data_hourly_schedule():
+    return {
+        "ops": {
+            "upload_asn_mmdb_to_s3": {
+                "config": {
+                    "enabled": True,
+                }
+            },
+            "upload_asn_sqlite_to_s3": {
+                "config": {
+                    "enabled": True,
+                }
+            },
+        },
+        "resources": {
+            "paths": {
+                "config": {
+                    "work_dir": "/var/lib/nossl",
+                    "bin_dir": "/opt/nossl/bin",
+                }
+            }
+        },
+    }
